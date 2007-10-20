f8d581435032f9411f865f75f89ddf5e08437b96
peter.backlund <peter.backlund@3b487c0d-713c-0410-a8ef-27d9d9722b01>
2007-10-20 17:53:08 +0000

    More validation fixes for Item, captcha rejection

--- trunk/grails-app/controllers/ItemController.groovy
+++ trunk/grails-app/controllers/ItemController.groovy
@@ -38,10 +38,9 @@ class ItemController {
         } else {
             item = new Item()
         }
-        item.price = 100
-        
-
-        //item.properties = params
+        if (params.price) {
+            item.price = params.price.toInteger()
+        }
 
         // TODO: need Grails 1.0 to bind more than one level deep
         Address address = new Address()
@@ -57,8 +56,6 @@ class ItemController {
         product.category = Category.get(params["product.category.id"])
         item.product = product
 
-        println "Product: " + item.product.properties
-
         SellerContactInfo contactInfo = new SellerContactInfo()
         ["firstName","lastName","email"].each {
             contactInfo.setProperty(it, params["contactInfo." + it])
@@ -66,7 +63,6 @@ class ItemController {
         item.contactInfo = contactInfo
 
         params.tagNames?.split("\\s").each {
-            println "TAG: " + it
             Tag tag = Tag.findByTag(it)
             if (tag) {
                 item.addToTags(tag)
@@ -74,13 +70,6 @@ class ItemController {
                 item.addToTags(new Tag(tag:it))
             }
         }
-
-
-        if (params[CAPTCHA_ATTR].trim() != session[CAPTCHA_ATTR]) {
-            item.errors.reject("captcha.mismatch")
-            //assert !item.validate()
-        }
-        //unsetCaptcha()
 
         def uploaded = request.getFile("file")
         if (!uploaded.empty) {
@@ -91,11 +80,16 @@ class ItemController {
         // TODO: set latitude & longitude
         //List gp = geoCoderService.geoCode(item.toString())
 
-        if (item.save()) {
+        item.validate()
+        if (params[CAPTCHA_ATTR]?.trim() != session[CAPTCHA_ATTR]) {
+            item.errors.reject("captchaMismatch")
+        }
+        if (!item.errors.hasErrors() && item.save()) {
             flash.message = "Saved item with id = " + item.id
+            unsetCaptcha()
             redirect(action:show,id:item.id)
         } else {
-            flash.message = "Validation errors"
+            flash.message = "${item.errors.errorCount} validation errors."
             render(view:"edit", model:[item:item])
         }
     }
