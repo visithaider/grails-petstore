08dfe16ff977d0cda02171fb9e92503ab45f11ed
peter.backlund <peter.backlund@3b487c0d-713c-0410-a8ef-27d9d9722b01>
2008-02-25 11:54:31 +0000

    Simplified tagAndSave a bit.

--- grails-app/services/org/grails/petstore/ItemService.groovy
+++ grails-app/services/org/grails/petstore/ItemService.groovy
@@ -12,15 +12,11 @@ class ItemService {
     static transactional = true
 
     synchronized boolean tagAndSave(Item item, List<String> tagsList) {
-        def geCodeTask = executorService.submit({
-            try {
-                def points = geoCoderService.geoCode(item.address.toString())
-                return points[0]
-            } catch (e) {
-                log.error "Failed to retrieve geocode for ${item.address}: ${e.message}"
-                return null
-            }
-        } as Callable)
+        def geoCodeTask = {
+            def points = geoCoderService.geoCode(item.address.toString())
+            return points[0]
+        } as Callable
+        def geCodeTask = executorService.submit(geoCodeTask)
 
         def tagSet = []
         tagsList.unique().each {
@@ -35,10 +31,8 @@ class ItemService {
 
         try {
             def gc = geCodeTask.get(1000, TimeUnit.MILLISECONDS)
-            if (gc) {
-                item.latitude = gc.latitude
-                item.longitude = gc.longitude
-            }
+            item?.latitude = gc.latitude
+            item?.longitude = gc.longitude
         } catch (e) {
             log.error """
                 Failed to get result of geocode task for address ${item.address},
