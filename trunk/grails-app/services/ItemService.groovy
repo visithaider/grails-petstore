4220aadcadc7316a3792f91205381a561195258b
peter.backlund <peter.backlund@3b487c0d-713c-0410-a8ef-27d9d9722b01>
2008-10-27 21:36:17 +0000

    Minor cleanup

--- trunk/grails-app/services/ItemService.groovy
+++ trunk/grails-app/services/ItemService.groovy
@@ -10,12 +10,12 @@ class ItemService {
     Destination coordinatesLookupQueue
 
     boolean fetchCoordinates = true
-    
+
     synchronized boolean tagAndSave(Item item, List<String> withTags) {
         tag item, withTags
         item.save()
         if (fetchCoordinates) {
-            jmsTemplate.convertAndSend(coordinatesLookupQueue, item.id.toString())
+            jmsTemplate.convertAndSend coordinatesLookupQueue, item.id.toString()
         }
     }
 
@@ -23,15 +23,15 @@ class ItemService {
 
        This method needs to be synchronized in order to avoid concurrent
        threads creating duplicate tags. */
+
     private void tag(Item item, List<String> tagsList) {
         def tagSet = []
         tagsList.unique().each {
             def t = Tag.findByTag(it)
             if (!t) {
-                t = new Tag(tag: it, items:[])
+                t = new Tag(tag: it, items: [])
                 t.save()
             }
-            //t.items.add(item)
             tagSet.add(t)
         }
         item.tags = tagSet as SortedSet
