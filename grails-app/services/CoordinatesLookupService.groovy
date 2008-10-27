dcc99ea8c88c4f5fe086086a073878014456beaa
peter.backlund <peter.backlund@3b487c0d-713c-0410-a8ef-27d9d9722b01>
2008-10-27 21:39:06 +0000

    Inverted if-condition

--- grails-app/services/CoordinatesLookupService.groovy
+++ grails-app/services/CoordinatesLookupService.groovy
@@ -8,22 +8,22 @@ class CoordinatesLookupService implements MessageListener {
     GeoCoderService geoCoderService
 
     void onMessage(Message message) {
-        def id = message.text.toLong() 
-        
-        def item = Item.get(id)
-        if (!item) {
-            log.error "No item with id ${id} found"
-        }
+        def id = message.text.toLong()
 
-        try {
-            def gc = geoCoderService.geoCode(item.address.toString())[0]
-            item.latitude = gc?.latitude
-            item.longitude = gc?.longitude
-            item.save()
-            log.debug "Retrieved coordinates: Latitude:${item.latitude}, Longitude:${item.longitude}"
-        } catch (e) {
-            log.error """Failed to get result of geocode task for address ${item.address},
-                         skipping coordinates: ${e}""", GrailsUtil.sanitize(e)
+        def item = Item.get(id)
+        if (item) {
+            try {
+                def gc = geoCoderService.geoCode(item.address.toString())[0]
+                item.latitude = gc?.latitude
+                item.longitude = gc?.longitude
+                item.save()
+                log.debug "Retrieved coordinates: Latitude:${item.latitude}, Longitude:${item.longitude}"
+            } catch (e) {
+                log.error """Failed to get result of geocode task for address ${item.address},
+                             skipping coordinates: ${e}""", GrailsUtil.sanitize(e)
+            }
+        } else {
+            log.error "No item with id ${id} found"
         }
     }
 }
